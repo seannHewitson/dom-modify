7ca22fc0de06097a9f86edb2b630e77ee2717e36
Sean Hewitson <sean.hewitson@therange.co.uk>
2020-06-03 10:35:51 +0100

    Ready for initial release.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = function(targetElement, repositionTarget = null){
-    if(!targetElement || !(repositionTarget instanceof Element))
-    return console.warn('Could not modify:', targetElement);
     
     var moveableArea = targetElement.querySelector(repositionTarget);
     
@@ -16,39 +14,101 @@ module.exports = function(targetElement, repositionTarget = null){
         right: false
     };
 
-    var starts = {
+    var start = {
         x: null,
         y: null,
-        top: parseInt(targetElement.offsetLeft),
-        left: parseInt(targetElement.offsetRight),
+        top: parseInt(targetElement.offsetTop),
+        left: parseInt(targetElement.offsetLeft),
         width: parseInt(targetElement.offsetWidth),
         height: parseInt(targetElement.offsetHeight)
     };
 
-    targetElement.addEventListener('mousedown', onMouseDown);
-    targetElement.addEventListener('mouseover', onMouseOver);
-    moveableArea.addEventListener('mousedown', onMouseDown);
+    targetElement.addEventListener('mousedown', onMouseDownResize);
+    targetElement.addEventListener('mouseover', onMouseOverResize);
+    moveableArea.addEventListener('mousedown', onMouseDownMove);
+    moveableArea.addEventListener('mouseover', onMouseOverMove);
 
     window.addEventListener('mousemove', onMouseMove);
     window.addEventListener('mouseup', onMouseUp);
 
-    function onMouseDown(event){
-        if(event.target === targetElement){
-            //  
+    function onMouseDownResize(event){
+        if(isMouseDown) return;
+        if(event.target === targetElement || event.target === moveableArea){
+            isMouseDown = true;
+            start.x = event.clientX;
+            start.y = event.clientY;
         }
     }
-    function onMouseOver(event){
-        if(event.target === targetElement){
-            mouseX = event.clientX;
-            mouseY = event.clientY;
-            scale.up = starts.y >= starts.top && starts.y <= (starts.top + 10);
-            scale.down = starts.y <= starts.top + targetElement.offsetHeight && starts.y >= ((starts.top + targetElement.offsetHeight) - 10);
-            scale.left = starts.x >= starts.left && starts.x <= (starts.left + 10);
-            scale.right = starts.x <= starts.left + targetElement.offsetWidth && starts.x >= ((starts.left + targetElement.offsetWidth) - 10);
+
+    function onMouseDownMove(event){
+        if(isMouseDown) return;
+        if(event.target === moveableArea || event.target.parentNode === moveableArea){
+            isMouseDown = true;
+            start.x = event.clientX;
+            start.y = event.clientY;
         }
-        var cursorString = `${scale.up ? 'n' : ''}${scale.down ? 's' : ''}${scale.left ? 'w' : ''}${scale.right ? 'e' : ''}`;
+    }
+
+    function onMouseOverResize(event){
+        if(isMouseDown) return;
+        var cursorString;
+        if(event.target === targetElement){
+            start.x = event.clientX;
+            start.y = event.clientY;
+            scale.up = start.y >= start.top && start.y <= (start.top + 10);
+            scale.down = start.y <= start.top + targetElement.offsetHeight && start.y >= ((start.top + targetElement.offsetHeight) - 10);
+            scale.left = start.x >= start.left && start.x <= (start.left + 10);
+            scale.right = start.x <= start.left + targetElement.offsetWidth && start.x >= ((start.left + targetElement.offsetWidth) - 10);
+            cursorString = `${scale.up ? 'n' : ''}${scale.down ? 's' : ''}${scale.left ? 'w' : ''}${scale.right ? 'e' : ''}`;
+        }
         targetElement.style.cursor = cursorString === '' ? 'default' : `${cursorString}-resize`;
     }
-    function onMouseMove(event){}
-    function onMouseUp(event){}
+
+    function onMouseOverMove(event){
+        if(isMouseDown) return;
+        if(event.target === moveableArea || event.target.parentNode === moveableArea){
+            targetElement.style.cursor = 'move';
+        } else {
+            targetElement.style.cursor = 'default';
+        }
+    }
+    
+    function onMouseMove(event){
+        if(!isMouseDown)    return;
+        var diff = {
+            x: event.clientX - start.x,
+            y: event.clientY - start.y
+        }
+        if(scale.up || scale.down || scale.left || scale.right){
+            if(scale.up || scale.down){
+                if(scale.up){
+                    targetElement.style.top = `${start.y + diff.y}px`;
+                    diff.y *= -1;
+                }
+                targetElement.style.height = `${start.height + diff.y}px`;
+            }
+            if(scale.left || scale.right){
+                if(scale.left){
+                    targetElement.style.left = `${start.x + diff.x}px`;
+                    diff.x *= -1;
+                }
+                targetElement.style.width = `${start.width + diff.x}px`;
+            }
+        } else {
+            targetElement.style.top = `${start.top + diff.y}px`;
+            targetElement.style.left = `${start.left + diff.x}px`;
+        }
+    }
+
+    function onMouseUp(event){
+        if(!isMouseDown)    return;
+        start.x = null;
+        start.y = null;
+        start.top = parseInt(targetElement.offsetTop);
+        start.left = parseInt(targetElement.offsetLeft);
+        start.width = parseInt(targetElement.offsetWidth);
+        start.height = parseInt(targetElement.offsetHeight);
+
+        scale.up = scale.down = scale.left = scale.right = isMouseDown = false;
+    }
 };
